9e7ffb3b60aa2639d19fe9a3226967f1484890cd
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-16 11:03:35 -0700

    Add: LoginViewController class and story object.

---
+++ iOSChatMessenger/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  iOSChatMessenger
+//
+//  Created by Jeremy Hartmann on 2016-03-16.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ iOSChatMessenger/LoginViewController.m
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.m
+//  iOSChatMessenger
+//
+//  Created by Jeremy Hartmann on 2016-03-16.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
