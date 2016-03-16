4b532ed66d133de21a35ad43e2141b7535c3454f
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-16 12:20:14 -0700

    Fix: Issue when loading login view.

--- iOSChatMessenger/AppDelegate.m
+++ iOSChatMessenger/AppDelegate.m
@@ -18,10 +18,11 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
-    UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
-    navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem;
-    splitViewController.delegate = self;
+    // TODO: Fix split view controller. 
+//    UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
+//    UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
+//    navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem;
+//    splitViewController.delegate = self;
     return YES;
 }
 
