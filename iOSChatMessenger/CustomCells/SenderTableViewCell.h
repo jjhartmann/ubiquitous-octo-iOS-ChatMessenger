e7e5be00e64c39de1a5132c117f43127db8c0a8e
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-17 11:41:50 -0700

    Add: Custom UITableViewCell for Sender

---
+++ iOSChatMessenger/CustomCells/SenderTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SenderTableViewCell.h
+//  iOSChatMessenger
+//
+//  Created by Jeremy Hartmann on 2016-03-17.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SenderTableViewCell : UITableViewCell
+
+@end
---
+++ iOSChatMessenger/CustomCells/SenderTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SenderTableViewCell.m
+//  iOSChatMessenger
+//
+//  Created by Jeremy Hartmann on 2016-03-17.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import "SenderTableViewCell.h"
+
+@implementation SenderTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
