9bcddbf0c7dfda511f6d7ec4277707400bb99d0c
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-17 11:45:36 -0700

    Add: Custom UITableViewCell for Receiver

---
+++ iOSChatMessenger/ReceiverTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ReceiverTableViewCell.h
+//  iOSChatMessenger
+//
+//  Created by Jeremy Hartmann on 2016-03-17.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReceiverTableViewCell : UITableViewCell
+
+@end
---
+++ iOSChatMessenger/ReceiverTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ReceiverTableViewCell.m
+//  iOSChatMessenger
+//
+//  Created by Jeremy Hartmann on 2016-03-17.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import "ReceiverTableViewCell.h"
+
+@implementation ReceiverTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
