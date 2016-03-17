57aafd9adbea3e152be151a96ae77e9c563db968
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-17 01:59:17 -0700

    Add: ChatClient to details view

--- iOSChatMessenger/MasterViewController.m
+++ iOSChatMessenger/MasterViewController.m
@@ -60,11 +60,13 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
         NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDate *object = self.objects[indexPath.row];
+        NSString *object = self.objects[indexPath.row];
         DetailViewController *controller = (DetailViewController *)[[segue destinationViewController] topViewController];
         [controller setDetailItem:object];
         controller.navigationItem.leftBarButtonItem = self.splitViewController.displayModeButtonItem;
         controller.navigationItem.leftItemsSupplementBackButton = YES;
+        controller.clientStream = self.clientStream;
+        controller.clientStream.delegate = controller;
     }
 }
 
