670d7695607aed45cfb0482685352d1f1b24c53b
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-17 18:20:31 -0700

    Update Cell UI

--- iOSChatMessenger/ReceiverTableViewCell.h
+++ iOSChatMessenger/ReceiverTableViewCell.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ReceiverTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundImageBubble;
+@property (weak, nonatomic) IBOutlet UILabel *messageLabel;
+@property (weak, nonatomic) IBOutlet UILabel *timestampLabel;
+@property (weak, nonatomic) IBOutlet UILabel *username;
 
 @end
