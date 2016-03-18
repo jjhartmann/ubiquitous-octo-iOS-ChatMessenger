928c798c61e0677ae75297dd1de233d50f66d5a2
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-18 01:34:39 -0700

    INIT: Data object class for messages.

---
+++ iOSChatMessenger/MessageObject.h
@@ -0,0 +1,13 @@
+//
+//  MessageObject.h
+//  iOSChatMessenger
+//
+//  Created by Jeremy Hartmann on 2016-03-18.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MessageObject : NSObject
+
+@end
---
+++ iOSChatMessenger/MessageObject.m
@@ -0,0 +1,13 @@
+//
+//  MessageObject.m
+//  iOSChatMessenger
+//
+//  Created by Jeremy Hartmann on 2016-03-18.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import "MessageObject.h"
+
+@implementation MessageObject
+
+@end
