56220bb65b70912981f2384cc0ebc1a7d5fc564f
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-18 01:40:18 -0700

    Add: properties to message data object.

--- iOSChatMessenger/MessageObject.h
+++ iOSChatMessenger/MessageObject.h
@@ -6,8 +6,17 @@
 //  Copyright © 2016 Jeremy Hartmann. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 
 @interface MessageObject : NSObject
+@property (strong, nonatomic) NSString *messageBody;
+@property (strong, nonatomic) NSString *messageTitle;
+@property (strong, nonatomic) NSString *usernameSender;
+@property (strong, nonatomic) NSString *usernameReceiver;
+@property (strong, nonatomic) UIImage *profileImage;
+@property BOOL isSender;
+@property BOOL hasURL;
+@property (strong, nonatomic) NSString *extractedURL;
 
 @end
