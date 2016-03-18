22ee119e6ca1f0bb6171db805fa20dc538056e41
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-18 14:27:24 -0700

    Add: Settings view controller

---
+++ iOSChatMessenger/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  iOSChatMessenger
+//
+//  Created by Jeremy Hartmann on 2016-03-18.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
