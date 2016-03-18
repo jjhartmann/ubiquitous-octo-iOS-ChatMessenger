e74742c4723090b1244f7336815c89423b825f96
Jeremy J Hartmann <jhartman@sfu.ca>
2016-03-18 00:30:30 -0700

    Add: Class AddGroupViewController
    Handles the adding of a new chat group

---
+++ iOSChatMessenger/AddGroupViewController.h
@@ -0,0 +1,14 @@
+//
+//  AddGroupViewController.h
+//  iOSChatMessenger
+//
+//  Created by Jeremy Hartmann on 2016-03-18.
+//  Copyright © 2016 Jeremy Hartmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddGroupViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *groupTextField;
+
+@end
